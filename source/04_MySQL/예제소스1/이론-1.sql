9c14f01d1011fa48036b7e1da007bdb7416bfb45
Harutoshi87 <rlaehdwnsxy@naver.com>
2026-01-22 14:37:13 +0900

    MySQL

---
+++ source/04_MySQL/예제소스1/이론-1.sql
@@ -0,0 +1,8 @@
+show databases;
+use sakila;
+show tables;
+show table status;
+select * from sakila.actor_info;
+select * from actor_info;
+select first_name, last_name from actor_info;
+desc actor_info;
