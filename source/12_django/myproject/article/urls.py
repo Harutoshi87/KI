6053ce285c337d104139ae33e8938949116df59c
Harutoshi87 <rlaehdwnsxy@naver.com>
2026-01-29 11:42:02 +0900

    django

--- source/12_django/myproject/article/urls.py
+++ source/12_django/myproject/article/urls.py
@@ -5,22 +5,14 @@ from . import views
 # 기사상세 /article/1/detail  article:detail
 # 기사수정 /article/1/edit/   article:edit
 # 기사삭제 /article/1/delete/ article:delete
-# v1. 검색기능 X
-from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
-from .models import Article
-from django.urls import reverse_lazy
+# v2. 검색기능 O, 파일첨부
+from .views import ArticleListView, ArticleCreateView, ArticleDetailView
+from .views import ArticleUpdateView, ArticleDeleteView
 app_name = "article"
 urlpatterns = [
-  # path("", views.article_list, name="list"),
-  # path("new/", views.article_new, name="new"),
-  # path("<int:pk>/detail", views.article_detail, name="detail"),
-  # path("<int:pk>/edit/", views.article_edit, name="edit"),
-  # path("<int:pk>/delete", views.article_delete, name="delete"),
-
-  path("", ListView.as_view(model=Article, paginate_by=3), name="list"),
-  path("new/", CreateView.as_view(model=Article, fields="__all__"), name="new"),
-  path("<int:pk>/detail", DetailView.as_view(model=Article), name="detail"),
-  path("<int:pk>/edit/", UpdateView.as_view(model=Article, fields="__all__"), name="edit"),
-  path("<int:pk>/delete", DeleteView.as_view(model=Article,
-                            success_url=reverse_lazy("article:list")), name="delete"),
+  path("", ArticleListView.as_view(), name="list"),
+  path("new/", ArticleCreateView.as_view(), name="new"),
+  path("<int:pk>/detail", ArticleDetailView.as_view(), name="detail"),
+  path("<int:pk>/edit/", ArticleUpdateView.as_view() , name="edit"),
+  path("<int:pk>/delete", ArticleDeleteView.as_view(), name="delete")
 ]
--- source/12_django/myproject/article/views.py
+++ source/12_django/myproject/article/views.py
@@ -6,19 +6,46 @@ from django.urls import reverse_lazy
 # /article : 1페이지   /article?page=3
 # def article_list(request):
 #   article_list = Article.objects.all()
+#   q = request.GET.get('q', '')
+#   if q:
+#     article_list = article_list.filter(title__icontains = q)
 #   paginator = Paginator(article_list, per_page=3)
 #   page_number = request.GET.get('page', '1')
 #   page_object = paginator.get_page(page_number)
 #   return render(request, "article/article_list.html",
 #                 {"article_list":page_object,
-#                 "page_obj":page_object})
-article_list = ListView.as_view(model=Article, paginate_by=3) # 한페이지에 3행씩
+#                 "page_obj":page_object,
+#                 "q":q})
+class ArticleListView(ListView):
+  model = Article
+  paginate_by = 3 # 한페이지당 3개씩 출력
+  def get_queryset(self):
+    article_list = super().get_queryset()
+    q = self.request.GET.get('q', '')
+    if q:
+      article_list = article_list.filter(title__icontains=q)
+    return article_list
+  def get_context_data(self, **kwargs):
+    context = super().get_context_data(**kwargs)
+    context['q'] = self.request.GET.get('q','')
+    return context
 
-article_new  = CreateView.as_view(model=Article, fields="__all__")
+class ArticleCreateView(CreateView):
+  model = Article
+  fields="__all__"
 
-article_detail  = DetailView.as_view(model=Article)
+from django.utils import timezone
+class ArticleDetailView(DetailView):
+  model = Article
+  def get_context_data(self, **kwargs):
+    context = super().get_context_data(**kwargs)
+    context['now'] = timezone.now()
+    return context
 
-article_edit  = UpdateView.as_view(model=Article, fields="__all__")
+class ArticleUpdateView(UpdateView):
+  model = Article
+  fields="__all__"
 
-article_delete  = DeleteView.as_view(model=Article,
-                                    success_url=reverse_lazy("article:list"))
+class ArticleDeleteView(DeleteView):
+  model=Article
+  success_url=reverse_lazy("article:list")
